1c66b3f1a69d65d0f04412d34c64264520c0e1f3
nikita <83126696+Aeromusician@users.noreply.github.com>
2024-04-20 01:54:03 +0300

    practices

--- tracksearch_service/app/main.py
+++ tracksearch_service/app/main.py
@@ -1,11 +1,54 @@
 import requests
 import uvicorn
 import os
-
-from fastapi import FastAPI, HTTPException, status
+from keycloak import KeycloakOpenID
+from fastapi import FastAPI, HTTPException, status, Form, Header
 
 app = FastAPI()
 api_key = api_key = "a7bc42ee80msh0d4ff0350852678p195558jsnf7e258d86683"
+
+KEYCLOAK_URL = "http://keycloak:8080/"
+KEYCLOAK_CLIENT_ID = "testClient"
+KEYCLOAK_REALM = "testRealm"
+KEYCLOAK_CLIENT_SECRET = "**********"
+
+user_token = ""
+keycloak_openid = KeycloakOpenID(server_url=KEYCLOAK_URL,
+                                  client_id=KEYCLOAK_CLIENT_ID,
+                                  realm_name=KEYCLOAK_REALM,
+                                  client_secret_key=KEYCLOAK_CLIENT_SECRET)
+
+
+
+from prometheus_fastapi_instrumentator import Instrumentator
+Instrumentator().instrument(app).expose(app)
+
+@app.post("/login")
+async def login(username: str = Form(...), password: str = Form(...)):
+    try:
+        token = keycloak_openid.token(grant_type=["password"],
+                                      username=username,
+                                      password=password)
+        return token
+    except Exception as e:
+        print(e)
+        raise HTTPException(status_code=400, detail="Не удалось получить токен")
+
+def user_is_musician(token):
+    try:
+        token_info = keycloak_openid.introspect(token)
+        if "test" not in token_info["realm_access"]["roles"]:
+            raise HTTPException(status_code=403, detail="Access denied")
+        return token_info
+    except Exception as e:
+        raise HTTPException(status_code=401, detail="Invalid token or access denied")
+
+@app.get("/health", status_code=status.HTTP_200_OK)
+async def service_alive(token: str = Header()):
+    if (user_is_musician(token)):
+        return {'message': 'service alive'}
+    else:
+        return "Wrong JWT Token"
 
 # Базовая функция для отправки запросов к Deezer API
 def search_deezer(query: str, search_type: str):
@@ -21,20 +64,18 @@ def search_deezer(query: str, search_type: str):
     else:
         return response.raise_for_status()
 
-@app.get("/health", status_code=status.HTTP_200_OK)
-async def service_alive():
-    return {'message': 'service alive'}
-
 # Эндпоинт для поиска по трекам
 @app.get("/search_tracks/")
-async def search_tracks(query: str):
-    try:
-        results = search_deezer(query, "track")
-        tracks = [{'artist': result['artist']['name'], 'track': result['title']} for result in results]
-        return tracks
-    except Exception as e:
-        raise HTTPException(status_code=404, detail=str(e))
-
+async def search_tracks(query: str, token: str = Header()):
+    if (user_is_musician(token)):
+        try:
+            results = search_deezer(query, "track")
+            tracks = [{'artist': result['artist']['name'], 'track': result['title']} for result in results]
+            return tracks
+        except Exception as e:
+            raise HTTPException(status_code=404, detail=str(e))
+    else:
+        return "Wrong JWT Token"
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=int(os.getenv('PORT', 80)))
